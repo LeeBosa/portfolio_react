3c4ce8387dcb26b5acfc42f4bc8965d4ce4f6f2f
taedonn <taedonn@outlook.com>
2023-04-06 10:48:48 +0900

    Add night mode

--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,19 @@
 import '../css/main.css';
 
 function Main() {
+  const handleNightMode = (e) => {
+    if(e.target.checked) { document.body.classList.replace('night_mode','bright_mode'); }
+    else { document.body.classList.replace('bright_mode','night_mode'); }
+  }
+
   return (
     <div className="main_index">
+      <div className='fixed_area'>
+        <input type='checkbox' id='color_mode' onChange={handleNightMode}/>
+        <label htmlFor='color_mode'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/></svg>
+        </label>
+      </div>
       <div className='index_left'>
         <div className='img_wrap'>
           <img src='https://i.ibb.co/GR9QpsK/profile.png' alt='프로필 사진'></img>
