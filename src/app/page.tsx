e6f6270d53bcfe973971313e04548c2da20a6362
taedonn <taedonn@taedonn.com>
2023-12-29 01:23:03 +0900

    Update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,11 +37,18 @@ export default function Home() {
       <Paragraph letterSpacing='wide'>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        <br/>
+        <br/>
         It has survived not only five centuries, but also the leap into electronic typesetting, 
         remaining essentially unchanged. It was popularised in the 1960s with the release of 
         Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
         like Aldus PageMaker including versions of Lorem Ipsum.
+        <br/>
+        <br/>
+        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
+        Sections 1.10.32 and 1.10.33 from &#34;de Finibus Bonorum et Malorum&#34; by Cicero are also reproduced 
+        in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
       </Paragraph>
       <Heading fontSize='3xl' marginTop={16} marginBottom={3}>Skills</Heading>
     </Main>
